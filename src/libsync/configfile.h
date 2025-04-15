37f2589f180394b82acdaeab87464bddc877de8d
Camila Ayres <hello@camilasan.com>
2025-04-15 12:51:42 +0200

    chore: make settings keys const static member of ConfigFile class.
    Signed-off-by: Camila Ayres <hello@camilasan.com>

--- src/libsync/configfile.h
+++ src/libsync/configfile.h
@@ -256,6 +256,14 @@ public:
     [[nodiscard]] static QString discoveredLegacyConfigPath();
     static void setDiscoveredLegacyConfigPath(const QString &discoveredLegacyConfigPath);
 
+    static const QString isVfsEnabledC;
+    static const QString launchOnSystemStartupC;
+    static const QString optionalServerNotificationsC;
+    static const QString promptDeleteC;
+    static const QString showCallNotificationsC;
+    static const QString showChatNotificationsC;
+    static const QString showInExplorerNavigationPaneC;
+
 protected:
     [[nodiscard]] QVariant getPolicySetting(const QString &policy, const QVariant &defaultValue = QVariant()) const;
     void storeData(const QString &group, const QString &key, const QVariant &value);
